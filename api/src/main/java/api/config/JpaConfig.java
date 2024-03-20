744068aea7acc3faea82828a1cd72e117288fe8a
cosmosaur <sjkang539@gmail.com>
2024-03-20 12:10:41 +0900

    add: jpa auditing 기능으로 생성시간 칼럼 추가

--- api/src/main/java/api/config/JpaConfig.java
+++ api/src/main/java/api/config/JpaConfig.java
@@ -2,9 +2,11 @@ package api.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaAuditing
 @EntityScan(basePackages = "db")
 @EnableJpaRepositories(basePackages = "db")
 public class JpaConfig {
--- db/src/main/java/db/entity/BaseEntity.java
+++ db/src/main/java/db/entity/BaseEntity.java
@@ -1,22 +1,28 @@
 package db.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @SuperBuilder
 @MappedSuperclass
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
