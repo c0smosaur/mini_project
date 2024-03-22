0ebe85cb4a9e8d73f841289f8b0b1c92df05c17c
cosmosaur <sjkang539@gmail.com>
2024-03-22 21:04:26 +0900

    Add #3: security와 swagger 설정 추가, application.yml 설정 파일

---
+++ api/src/main/java/api/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package api.config;
+
+import api.config.auth.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // httpBasic 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // csrf 토큰 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                // form login 비활성화
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        it -> it
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .requestMatchers("/open-api/**").permitAll()
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                // STATELESS 설정
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // JWT 필터 추가
+                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
+        ;
+
+        return httpSecurity.build();
+    }
+}
