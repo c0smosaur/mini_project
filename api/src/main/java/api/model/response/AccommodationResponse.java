515e602f341ad1b624102109a53a5d74e0633832
박하은 <deveun23@gmail.com>
2024-03-22 22:09:11 +0900

    Add #6 : AccommodationResponse 생성

--- api/src/main/java/api/model/response/AccommodationResponse.java
+++ api/src/main/java/api/model/response/AccommodationResponse.java
@@ -1,4 +1,18 @@
 package api.model.response;
 
+import db.enums.AccommodationCategory;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
 public class AccommodationResponse {
+    private String title;
+    private String address;
+    private String image1;
+    private String image2;
+    private String description;
+    private AccommodationCategory category;
 }
