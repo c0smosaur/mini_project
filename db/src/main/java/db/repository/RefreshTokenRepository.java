bd9d1e80b1a8bc12c71fbd94bcc448365aa7ed84
cosmosaur <sjkang539@gmail.com>
2024-03-22 20:56:26 +0900

    Add #3: TokenDTO, RefreshTokenConverter와 Repository 추가

--- db/src/main/java/db/repository/RefreshTokenRepository.java
+++ db/src/main/java/db/repository/RefreshTokenRepository.java
@@ -3,5 +3,11 @@ package db.repository;
 import db.entity.RefreshTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
+
+    Optional<RefreshTokenEntity> findFirstByToken(String token);
+
+    Optional<RefreshTokenEntity> findFirstByMemberId(Long memberId);
 }
