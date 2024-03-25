a992dc8288b922b01605d81b63610a95bf38c581
박하은 <deveun23@gmail.com>
2024-03-25 16:19:59 +0900

    Modify #6 : AccommodationEntity에 tel, latitude, longitude 컬럼 추가

--- db/src/main/java/db/entity/AccommodationEntity.java
+++ db/src/main/java/db/entity/AccommodationEntity.java
@@ -5,16 +5,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity(name = "accommodation")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class AccommodationEntity extends BaseEntity{
+@EqualsAndHashCode
+public class AccommodationEntity extends BaseEntity {
 
     @Column(nullable = false, length = 50)
     private String title;
@@ -35,4 +38,10 @@ public class AccommodationEntity extends BaseEntity{
     @Column(nullable = false, length = 50)
     private AccommodationCategory category;
 
+    @Column(length = 50)
+    private String tel;
+
+    private Double latitude;
+
+    private Double longitude;
 }
