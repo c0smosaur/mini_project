9b925b72422075e633580bb8ba400b551494516a
cosmosaur <sjkang539@gmail.com>
2024-03-26 18:15:51 +0900

    Add #12: ErrorCode 추가(member, reservation)

--- api/src/main/java/api/common/error/MemberErrorCode.java
+++ api/src/main/java/api/common/error/MemberErrorCode.java
@@ -9,6 +9,7 @@ public enum MemberErrorCode implements ErrorCodeMold{
 
     DUPLICATE_USERNAME(400,2100, "중복 이메일 주소"),
     IMAGE_ERROR(400,2101,"프로필 이미지 오류"),
+    USER_DOES_NOT_EXIST(400, 2102,"존재하지 않는 유저"),
     ;
 
     private final Integer httpStatusCode;
---
+++ api/src/main/java/api/common/error/ReservationErrorCode.java
@@ -0,0 +1,19 @@
+package api.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ReservationErrorCode implements ErrorCodeMold{
+
+    NULL_RESERVATION(400, 2201,"빈 예약"),
+    BAD_REQUEST(400, 2202, "잘못된 요청"),
+    WRONG_DATE(400,2203,"날짜 오류"),
+    ;
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private String description;
+
+}
