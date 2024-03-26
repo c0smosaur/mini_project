9029ff9dc650e325da5ab9f04045128e51ccc253
cosmosaur <sjkang539@gmail.com>
2024-03-26 11:33:30 +0900

    Modify #3: 회원가입 요청 JsonNamingStrategy 수정

--- api/src/main/java/api/model/request/MemberRegisterRequest.java
+++ api/src/main/java/api/model/request/MemberRegisterRequest.java
@@ -1,5 +1,7 @@
 package api.model.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class) // profile_image에 적용이 안되길래 일단 추가
 public class MemberRegisterRequest {
 
     @NotBlank
