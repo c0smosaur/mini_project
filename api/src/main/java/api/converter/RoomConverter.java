b5bdc710e5db459e1cc65aee9525377b132ca4de
박하은 <deveun23@gmail.com>
2024-03-26 18:23:57 +0900

    Add #14 : RoomConverter 생성

--- api/src/main/java/api/converter/RoomConverter.java
+++ api/src/main/java/api/converter/RoomConverter.java
@@ -1,4 +1,17 @@
 package api.converter;
 
+import api.common.annotation.Converter;
+import api.model.response.RoomResponse;
+import db.entity.RoomEntity;
+
+@Converter
 public class RoomConverter {
+    public RoomResponse toResponse(RoomEntity entity) {
+        return RoomResponse.builder()
+                .id(entity.getId())
+                .maxCapacity(entity.getMaxCapacity())
+                .price(entity.getPrice())
+                .stock(entity.getStock())
+                .build();
+    }
 }
