37d80afea19bfd61739cdba0af0e709baa987d60
cosmosaur <sjkang539@gmail.com>
2024-03-26 18:16:11 +0900

    Add #12: Reservation 관련 엔티티와 DTO 추가

--- api/src/main/java/api/model/request/ReservationRequest.java
+++ api/src/main/java/api/model/request/ReservationRequest.java
@@ -1,4 +1,24 @@
 package api.model.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ReservationRequest {
+
+    private Long memberId;
+    private Long roomId;
+    private Integer capacity;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal totalPrice;
 }
--- api/src/main/java/api/model/response/ReservationResponse.java
+++ api/src/main/java/api/model/response/ReservationResponse.java
@@ -1,4 +1,24 @@
 package api.model.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ReservationResponse {
+
+    private Long id;
+    private Long roomId;
+    private Integer capacity;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal totalPrice;
+
 }
