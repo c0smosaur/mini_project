6ab5a67be349d23823fec440fab0b9137f22c2f5
cosmosaur <sjkang539@gmail.com>
2024-03-26 18:17:25 +0900

    Add #12: Reservation 관련 로직 추가(repository, service, controller)

--- api/src/main/java/api/controller/ReservationController.java
+++ api/src/main/java/api/controller/ReservationController.java
@@ -1,4 +1,49 @@
 package api.controller;
 
+import api.common.result.ResultWrapper;
+import api.model.request.ReservationRequest;
+import api.model.response.ReservationResponse;
+import api.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservations")
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    // 결제 후 reservation 저장
+    @PostMapping("/payments")
+    public ResultWrapper<ReservationResponse> addReservation(
+            @RequestBody ReservationRequest request) {
+        ReservationResponse response = reservationService.addReservation(request);
+        return ResultWrapper.OK(response);
+    }
+
+    // 특정 방의 예약 조회
+    @GetMapping("/room/{room-id}")
+    public ResultWrapper<List<ReservationResponse>> viewReservationForRoom(
+            @PathVariable(name = "room-id") Long roomId){
+        List<ReservationResponse> responseList = reservationService.getAllReservationForRoom(roomId);
+        return ResultWrapper.OK(responseList);
+    }
+
+    // 예약 내역 확인
+    @GetMapping("/{reservationId}")
+    public ResultWrapper<ReservationResponse> confirmReservation(
+            @PathVariable Long reservationId){
+        ReservationResponse response = reservationService.getReservation(reservationId);
+        return ResultWrapper.OK(response);
+    }
+
+    // 사용자의 과거 예약 내역 전체 조회
+    @GetMapping("/history")
+    public ResultWrapper<List<ReservationResponse>> viewUserReservationsAll(){
+        List<ReservationResponse> responseList = reservationService.getUserReservationAll();
+        return ResultWrapper.OK(responseList);
+    }
 }
--- api/src/main/java/api/service/ReservationService.java
+++ api/src/main/java/api/service/ReservationService.java
@@ -1,4 +1,101 @@
 package api.service;
 
+import api.common.error.MemberErrorCode;
+import api.common.error.ReservationErrorCode;
+import api.common.exception.ResultException;
+import api.converter.ReservationConverter;
+import api.model.request.ReservationRequest;
+import api.model.response.ReservationResponse;
+import db.entity.MemberEntity;
+import db.entity.ReservationEntity;
+import db.enums.MemberStatus;
+import db.repository.MemberRepository;
+import db.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+    private final ReservationConverter reservationConverter;
+    private final MemberRepository memberRepository;
+
+    // 날짜 유효성 검증
+    public boolean validateStartAndEndDate(ReservationRequest request){
+        LocalDate startDate = request.getStartDate();
+        LocalDate endDate = request.getEndDate();
+
+        // startDate가 endDate보다 전 (O)
+        if (startDate.isBefore(endDate)){
+            LocalDate currentDate = LocalDate.now();
+            // 오늘 날짜가 startDate보다 전이거나 같음
+            return currentDate.isBefore(startDate) || currentDate.isEqual(startDate);
+        }
+        // startDate가 endDate보다 나중 (X)
+        return false;
+    }
+
+    // 예약 추가
+    public ReservationResponse addReservation(ReservationRequest request) {
+        // 현재 유저 식별번호 받아옴
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MemberEntity memberEntity = memberRepository.findFirstByUsernameAndStatus(
+                authentication.getName(),
+                MemberStatus.REGISTERED
+        ).orElseThrow(() -> new ResultException(MemberErrorCode.USER_DOES_NOT_EXIST));
+
+        // 예약 날짜 유효성 확인
+        if (!validateStartAndEndDate(request)){
+            throw new ResultException(ReservationErrorCode.WRONG_DATE);
+        };
+
+        // member id 넣어주기
+        request.setMemberId(memberEntity.getId());
+
+        ReservationEntity entity = reservationConverter.toEntity(request);
+        ReservationEntity newEntity = reservationRepository.save(entity);
+        ReservationResponse response = reservationConverter.toResponse(newEntity);
+        return response;
+    }
+
+    // 숙소 객실별 예약 조회
+    public List<ReservationResponse> getAllReservationForRoom(Long roomId){
+        List<ReservationEntity> list = reservationRepository.findAllByRoomIdOrderByCreatedAtDesc(roomId);
+        List<ReservationResponse> responseList = list.stream().map(reservationConverter::toResponse).toList();
+        return responseList;
+    }
+
+    // 단일 예약 조회
+    public ReservationResponse getReservation(Long reservationId){
+        ReservationEntity entity = reservationRepository.findFirstById(reservationId)
+                .orElseThrow(() -> new ResultException(ReservationErrorCode.NULL_RESERVATION));
+
+        ReservationResponse response = reservationConverter.toResponse(entity);
+        return response;
+    }
+
+    // 사용자의 예약 전체 조회
+    public List<ReservationResponse> getUserReservationAll(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MemberEntity memberEntity = memberRepository.findFirstByUsernameAndStatus(
+                authentication.getName(),
+                MemberStatus.REGISTERED
+        ).orElseThrow(() -> new ResultException(MemberErrorCode.USER_DOES_NOT_EXIST));
+
+        List<ReservationEntity> list = reservationRepository.findAllByMemberIdOrderByCreatedAtDesc(memberEntity.getId());
+        List<ReservationResponse> responseList = list.stream()
+                .map(reservationConverter::toResponse).toList();
+
+        return responseList;
+    }
+
+    // cart로 전달받아 reservation에 추가
+
 }
