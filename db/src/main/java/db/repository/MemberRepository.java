4f30580b714bf020307bcc74e7fbc23aeb120f69
Seo Jean Kang <119040660+c0smosaur@users.noreply.github.com>
2024-03-27 18:19:31 +0900

    Modify #22: MemberRepository 수정

--- db/src/main/java/db/repository/MemberRepository.java
+++ db/src/main/java/db/repository/MemberRepository.java
@@ -3,7 +3,6 @@ package db.repository;
 import db.entity.MemberEntity;
 import db.enums.MemberStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
