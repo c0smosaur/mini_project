211e68ef0ee7c3f8db79a2779c8c1fab0ae7e4fa
Seo Jean Kang <119040660+c0smosaur@users.noreply.github.com>
2024-03-27 11:29:45 +0900

    Add: 현재 로그인 유저 정보 db서 받아오는 메서드 util로 분리

---
+++ api/src/main/java/api/common/util/MemberUtil.java
@@ -0,0 +1,29 @@
+package api.common.util;
+
+
+import api.common.error.TokenErrorCode;
+import api.common.exception.ResultException;
+import db.entity.MemberEntity;
+import db.enums.MemberStatus;
+import db.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MemberUtil {
+
+    private final MemberRepository memberRepository;
+
+    public MemberEntity getCurrentMember(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MemberEntity memberEntity = memberRepository.findFirstByUsernameAndStatus(
+                authentication.getName(),
+                MemberStatus.REGISTERED
+        ).orElseThrow(() -> new ResultException(TokenErrorCode.TOKEN_EXCEPTION));
+
+        return memberEntity;
+    }
+}
