d450a81ea32ff2b8970202eb1b6ca524b3116529
cosmosaur <sjkang539@gmail.com>
2024-03-28 18:04:28 +0900

    Modify #25: room repository 메서드 추가

--- db/src/main/java/db/repository/RoomRepository.java
+++ db/src/main/java/db/repository/RoomRepository.java
@@ -4,7 +4,10 @@ import db.entity.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<RoomEntity, Long> {
     List<RoomEntity> findAllByAccommodationId(Long accommodationId);
+
+    Optional<RoomEntity> findFirstById(Long id);
 }
