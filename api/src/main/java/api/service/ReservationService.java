2572ac25e7381d5e8b5fe0cdef22b5774345f78a
cosmosaur <sjkang539@gmail.com>
2024-03-29 16:35:18 +0900

    Modify #29: AccoommodationReservationResponse 반환하도록 수정

--- api/src/main/java/api/service/ReservationService.java
+++ api/src/main/java/api/service/ReservationService.java
@@ -3,22 +3,33 @@ package api.service;
 import api.common.error.ReservationErrorCode;
 import api.common.exception.ResultException;
 import api.common.util.MemberUtil;
+import api.converter.AccommodationConverter;
 import api.converter.ReservationConverter;
+import api.model.response.AccommodationReservationResponse;
+import api.model.response.AccommodationResponse;
 import api.model.response.ReservationResponse;
+import db.entity.AccommodationEntity;
 import db.entity.MemberEntity;
 import db.entity.ReservationEntity;
+import db.entity.RoomEntity;
+import db.repository.AccommodationRepository;
 import db.repository.ReservationRepository;
+import db.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final AccommodationRepository accommodationRepository;
+    private final RoomRepository roomRepository;
     private final ReservationConverter reservationConverter;
+    private final AccommodationConverter accommodationConverter;
     private final MemberUtil memberUtil;
 
 
@@ -30,22 +41,40 @@ public class ReservationService {
     }
 
     // 단일 예약 조회
-    public ReservationResponse getReservation(Long reservationId){
-        ReservationEntity entity = reservationRepository.findFirstById(reservationId)
+    public AccommodationReservationResponse getReservation(Long reservationId){
+        ReservationEntity reservationEntity = reservationRepository.findFirstById(reservationId)
+                .orElseThrow(() -> new ResultException(ReservationErrorCode.NULL_RESERVATION));
+        RoomEntity roomEntity = roomRepository.findFirstById(reservationEntity.getRoomId())
+                .orElseThrow(() -> new ResultException(ReservationErrorCode.NULL_RESERVATION));
+        AccommodationEntity accommodationEntity = accommodationRepository.findFirstById(roomEntity.getAccommodationId())
                 .orElseThrow(() -> new ResultException(ReservationErrorCode.NULL_RESERVATION));
 
-        ReservationResponse response = reservationConverter.toResponse(entity);
+        AccommodationResponse accommodation = accommodationConverter.toResponse(accommodationEntity);
+        AccommodationReservationResponse response = reservationConverter.toResponse(
+                accommodation, reservationEntity, roomEntity);
+
         return response;
     }
 
     // 사용자의 예약 전체 조회
-    public List<ReservationResponse> getUserReservationAll(){
+    public List<AccommodationReservationResponse> getUserReservationAll(){
         MemberEntity memberEntity = memberUtil.getCurrentMember();
 
         List<ReservationEntity> list = reservationRepository.findAllByMemberIdOrderByCreatedAtDesc(memberEntity.getId());
-        List<ReservationResponse> responseList = list.stream()
-                .map(reservationConverter::toResponse).toList();
+//        List<ReservationResponse> responseList = list.stream()
+//                .map(reservationConverter::toResponse).toList();
 
-        return responseList;
+        return list.stream()
+                .map(reservationEntity -> {
+                    RoomEntity roomEntity = roomRepository.findFirstById(reservationEntity.getRoomId())
+                            .orElseThrow(() -> new ResultException(ReservationErrorCode.NULL_RESERVATION));
+                    AccommodationEntity accommodationEntity = accommodationRepository.findFirstById(roomEntity.getAccommodationId())
+                            .orElseThrow(() -> new ResultException(ReservationErrorCode.NULL_RESERVATION));
+
+                    AccommodationResponse accommodation = accommodationConverter.toResponse(accommodationEntity);
+                    return reservationConverter.toResponse(
+                            accommodation, reservationEntity, roomEntity);
+                })
+                .collect(Collectors.toList());
     }
 }
