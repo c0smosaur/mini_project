0e035fefcce2fafbb7fac650f183ffb6eb154203
ndy <ratcomp9992@gmail.com>
2024-03-29 23:16:53 +0900

    Revert "Modify #21: allowedOriginPatterns()로 변경"
    This reverts commit f856da8f0a6c34d2a13246b2c3fa925035aa8534.

--- api/src/main/java/api/config/WebConfig.java
+++ api/src/main/java/api/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 허용
-                .allowedOriginPatterns("*")  // 모든 오리진 허용
+                .allowedOrigins("*")  // 모든 오리진 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드 지정
                 .allowedHeaders("*")      // 모든 헤더 허용
                 .allowCredentials(true);    // JWT 사용을 위한 자격 증명 허용
