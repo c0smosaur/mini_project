cd31d558ac0b7c0507e4711a020738044fac779f
cosmosaur <sjkang539@gmail.com>
2024-03-29 18:10:49 +0900

    Modify: member api url 수정

--- api/src/main/java/api/controller/MemberApiController.java
+++ api/src/main/java/api/controller/MemberApiController.java
@@ -2,21 +2,35 @@ package api.controller;
 
 import api.common.result.ResultWrapper;
 import api.config.jwt.JwtProvider;
+import api.model.request.MemberLoginRequest;
+import api.model.request.MemberRegisterRequest;
+import api.model.response.MemberLoginResponse;
 import api.model.response.MemberResponse;
 import api.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
+@RequestMapping("/api/members")
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @PostMapping("/register")
+    public ResultWrapper<MemberResponse> register(
+            @RequestBody MemberRegisterRequest request) {
+        MemberResponse response = memberService.register(request);
+        return ResultWrapper.OK(response);
+    }
+
+    @PostMapping("/login")
+    public ResultWrapper<MemberLoginResponse> signIn(
+            @RequestBody MemberLoginRequest request) {
+        MemberLoginResponse response = memberService.signIn(request);
+        return ResultWrapper.OK(response);
+    }
 
     @GetMapping("/my-page")
     public ResultWrapper<MemberResponse> memberInfo(){
@@ -25,7 +39,7 @@ public class MemberApiController {
     }
 
     @PostMapping("/logout")
-    public ResultWrapper logout(){
+    public ResultWrapper<Void> logout(){
         memberService.memberLogout();
         return ResultWrapper.OK(null);
     }
