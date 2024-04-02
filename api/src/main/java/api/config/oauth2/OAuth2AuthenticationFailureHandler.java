160a43531a1e4de8d415986f59e7cfb81f3c9de9
cosmosaur <sjkang539@gmail.com>
2024-04-02 17:05:14 +0900

    Modify: 테스트 필요

--- api/src/main/java/api/config/oauth2/OAuth2AuthenticationFailureHandler.java
+++ api/src/main/java/api/config/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -1,8 +1,37 @@
 package api.config.oauth2;
 
+import api.common.util.CookieUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.naming.AuthenticationException;
+import java.io.IOException;
+
+import static api.config.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 @Component
+@RequiredArgsConstructor
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final HttpCookieOAuth2AuthorizationRequestRepository cookieOAuth2AuthorizationRequestRepository;
+
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException authenticationException) throws IOException {
+        String url = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse("/");
+
+        url = UriComponentsBuilder.fromUriString(url)
+                .queryParam("error", authenticationException.getLocalizedMessage())
+                .build().toUriString();
+
+        cookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+        getRedirectStrategy().sendRedirect(request, response, url);
+    }
 }
