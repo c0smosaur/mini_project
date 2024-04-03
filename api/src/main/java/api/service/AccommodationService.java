ad155b19bbbe7811cbfedee043de1697c5070afd
박하은 <deveun23@gmail.com>
2024-04-03 15:31:27 +0900

    Modify #33 : 구조에 맞게 로직 수정

--- api/src/main/java/api/service/AccommodationService.java
+++ api/src/main/java/api/service/AccommodationService.java
@@ -3,14 +3,10 @@ package api.service;
 import api.common.error.GeneralErrorCode;
 import api.common.exception.ResultException;
 import api.converter.AccommodationConverter;
-import api.converter.RoomConverter;
-import api.model.response.AccommodationDetailResponse;
 import api.model.response.AccommodationResponse;
-import api.model.response.RoomResponse;
 import db.entity.AccommodationEntity;
 import db.enums.AccommodationCategory;
 import db.repository.AccommodationRepository;
-import db.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,9 +20,6 @@ import java.util.List;
 public class AccommodationService {
     private final AccommodationRepository accommodationRepository;
     private final AccommodationConverter accommodationConverter;
-
-    private final RoomRepository roomRepository;
-    private final RoomConverter roomConverter;
 
     // 숙소 전체 조회
     @Transactional(readOnly = true)
@@ -48,25 +41,13 @@ public class AccommodationService {
 
     // 숙소 개별 조회 (상세 조회)
     @Transactional(readOnly = true)
-    public AccommodationDetailResponse getAccommodationById(Long id) {
+    public AccommodationResponse getAccommodationById(Long id) {
         var optional = accommodationRepository.findById(id);
         AccommodationEntity accommodation = optional
                 .orElseThrow(
                         () -> new ResultException(GeneralErrorCode.NOT_FOUND)
                 );
 
-        return AccommodationDetailResponse.builder()
-                .accommodation(accommodationConverter.toResponse(accommodation))
-                .rooms(getAllRoomsByAccommodationId(id))
-                .build();
-    }
-
-    // 숙소 id에 해당하는 방 전체 조회
-    @Transactional(readOnly = true)
-    public List<RoomResponse> getAllRoomsByAccommodationId(Long accommodationId) {
-        return roomRepository.findAllByAccommodationId(accommodationId)
-                .stream()
-                .map(roomConverter::toResponse)
-                .toList();
+        return accommodationConverter.toResponse(accommodation);
     }
 }
