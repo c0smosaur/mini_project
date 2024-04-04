dd9317c8a6ac60cb40e118b880eef6ff9f6f44b6
박하은 <deveun23@gmail.com>
2024-04-04 20:16:48 +0900

    Modify #23 : @SpringBootApplication을 흡수시키기 위해 @Import 추가

--- api/src/main/java/api/MiniProjectApplication.java
+++ api/src/main/java/api/MiniProjectApplication.java
@@ -1,9 +1,12 @@
 package api;
 
+import db.DBApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(DBApplication.class)
 public class MiniProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(MiniProjectApplication.class, args);
